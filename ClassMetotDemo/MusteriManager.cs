5612af3ab4c749dc9c8c3d95f3513a33b7ca4ee3
Faruk Tekbaş <faruktekbas60@gmail.com>
2021-12-23 10:30:37 +0300

    Odevli

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void GetAll(Musteri[] musteriler)
+        {
+            foreach (var item in musteriler)
+            {
+                Console.WriteLine("{0} kişisi ", item.Ad);
+            }
+        }
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("{0} Eklendi", musteri.Ad);
+        }
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine("{0} Yenilendi", musteri.Ad);
+        }
+        public void Delete(Musteri musteri)
+        {
+            Console.WriteLine("{0} Silindi", musteri.Ad);
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassMetotDemo;
+
+Console.WriteLine("Hello, World!");
+Musteri musteri1 = new Musteri();
+musteri1.Id = 1;
+musteri1.Ad = "Faruk";
+musteri1.Soyad = "Tekbas";
+musteri1.Bakiye = 1250;
+Musteri musteri2 = new Musteri();
+musteri2.Id = 2;
+musteri2.Ad = "Kemal";
+musteri2.Soyad = "Aslan";
+musteri2.Bakiye = 350000;
+
+
+Musteri[] musteriler = new Musteri[] { musteri1, musteri2};
+
+MusteriManager musteriManager = new MusteriManager();
+musteriManager.GetAll(musteriler);
+musteriManager.Add(musteri1);
+musteriManager.Update(musteri1);
+musteriManager.Delete(musteri1);
+
