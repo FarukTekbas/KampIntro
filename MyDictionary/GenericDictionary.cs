9e9c8a98aedbe798a39876ae5412878edc128fa9
Faruk Tekbaş <faruktekbas60@gmail.com>
2021-12-24 14:03:32 +0300

    Odev 4

---
+++ MyDictionary/GenericDictionary.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class GenericDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public GenericDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] keyTempArray = keys;
+            TValue[] valueTempArray = values;
+
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+
+            for (int i = 0; i < keyTempArray.Length; i++)
+            {
+                keys[i] = keyTempArray[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < valueTempArray.Length; i++)
+            {
+                values[i] = valueTempArray[i];
+            }
+            values[values.Length - 1] = value;
+        }
+        public int Length
+        {
+            get { return keys.Length; }
+        }
+        public TValue[] Values
+        {
+            get { return values; }
+        }
+        public TKey[] Keys
+        {
+            get { return keys; }
+        }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyDictionary;
+
+Console.WriteLine("Hello, World!");
+
+
+GenericDictionary<int, string> myDictionary = new GenericDictionary<int, string>();
+
+myDictionary.Add(1, "Faruk");
+myDictionary.Add(2, "Ahsen");
+
+Console.WriteLine("Total Length: " + myDictionary.Length);
+
+foreach (var value in myDictionary.Values)
+{
+    Console.WriteLine("Value: " + value);
+}
+
+
+
+
+
+
+
